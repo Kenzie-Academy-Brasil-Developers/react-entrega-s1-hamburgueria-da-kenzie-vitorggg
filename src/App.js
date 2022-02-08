c1514fa1aa9aee9031ba7af7dffce1a69d6004af
Vitor Geraldi <vitor.geraldi.g@gmail.com>
2022-02-08 11:40:44 -0300

    feat: App is now catching the list of products from API

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentSale, setCurrentSale] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    fetch(`https://hamburgueria-kenzie-json-serve.herokuapp.com/products`)
+      .then((response) => response.json())
+      .then((response) => setProducts(response))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(products);
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
